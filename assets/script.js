5f51438656f3ed6723835764519835ecb3987e9e
sentuhxd <142236429+sentuhxd@users.noreply.github.com>
2023-08-29 21:18:20 -0400

    added readme

--- assets/script.js
+++ assets/script.js
@@ -6,98 +6,120 @@ var playnowBtn = document.querySelector("#play-start");
 var endplayBtn = document.querySelector("#end-play");
 var initalEl = document.querySelector("#initial");
 var questionsEl = document.querySelector("#question-sentences");
-var infoEl= document.querySelector("#info")
-var responseEl = document.querySelector("#response")
+var choicesEl = document.querySelector("#choices");
+var responseEl = document.querySelector("#response");
+var nameEl = document.querySelector("#name");
 
 
 
 
 
-var questions = [  
+var questions = [
     {
-      question: "What kind of language is JavaScript?",
-      options: ["Hypertext Markup Language", "Java", "Object-Oriented", "Cascading Style Sheets"],
-      correct: "Object-Oriented"
-    },
-    {
-      question: "Which function is used to serialize an object into JSON string in JavaScript?",
-      options: ["parse()", "stringify()", "array()", "parseInt()"],
-      correct: "stringify()"
-    },
-    {
-      question: "How do you call a function named myFunction?",
-      options: ["call myFunction()", "myFunction()", "call function myFunction", "Call.myFunction"],
-      correct: "myFunction()"
-    },
-    {
-      question: "Which of the following is not a method?",
-      options: ["toLowerCase()", ".length", "push()", "sort()"],
-      correct: ".length"
-    },
-]
+        question: "What kind of language is JavaScript?",
+        choices: ["Hypertext Markup Language", "Object-based", "Object-Oriented", "Cascading Style Sheet"],
+        answer: "Object-Oriented"
+      },
+      {
+        question: "Which function is used to serialize an object into JSON string in JavaScript?",
+        choices: ["parse()", "stringify()", "array()", "parseInt()"],
+        answer: "stringify()"
+      },
+      {
+        question: "Which of the following is a JavaScript framework?",
+        choices: ["Spark", "Django", "Spring", "React"],
+        answer: "React"
+      },
+      {
+        question: "Which of the following is not a method?",
+        choices: ["toLowerCase()", ".length", "push()", "sort()"],
+        answer: ".length"
+      },
+] 
+
 
 // DATA 
-var timeLeft = questions.length * 10;
-var timeID;
+
 var qIndex = 0;
+var time = questions.length * 15;
+var timerID;
 
 
 //Functions
 
 function gameStart() {
-    timeID = setInterval(timeDown, 1000);
-    timerEl.textContent = timeLeft;
-    var containerScreenEl = document.getElementById("container-screen");
-    containerScreenEl.setAttribute("class", "hide");
+    timerID = setInterval(timeDown, 1000);
+    timerEl.textContent = time;
+    var startScreenEl = document.getElementById("container-screen");
+    startScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
-    
     newQuestion();
 }
 
 
 
-function timeDown() {
-    time--;
-    timerEl.textContent = time;
-    if (time <= 0) {
-        quizEnd();
-    }
-}
+
 
 function newQuestion() {
-    var nowQuestion = questions[qIndex]
-    var questionSenEl = document.getElementById("question-sentences")
-    questionSenEl.textContent = nowQuestion.prompt;
-    infoEl.innerHTML = "";
-    
-    nowQuestion.options.forEach(function(info, i) {
-        var selectBtn = document.createElement("button");
-        selectBtn.setAttribute("value", info);
-        selectBtn.textContent= i + 1 + ". " + info;
-        selectBtn.onclick = questionChoice; 
-        infoEl.appendChild(selectBtn);
+    var nowQuestion = questions[qIndex];
+    var promptEl = document.getElementById("question-sentences");
+    promptEl.textContent = nowQuestion.question;
+    choicesEl.innerHTML = "";
+
+    nowQuestion.options.forEach(function(choices, i) {
+        var choicesBtn = document.createElement("button");
+        choicesBtn.setAttribute("value", choices);
+        choicesBtn.textContent= i + 1 + ". " + choices;
+        choicesBtn.onclick = questionChoice; 
+        choicesEl.appendChild(choicesBtn);
 
     });
 }
 
 function questionChoice() {
-    if (this.value !== questions[qIndex].correct) {
-        time -=15;
+    if (this.value !== questions[qIndex].answer) {
+        time -= 10;
         if (time < 0 ) {
             time = 0;
         }
         timerEl.textContent = time;
-        responseEl.textContent = "WRONG D:";
+        responseEl.textContent = `Wrong! The correct answer was ${questions[qIndex].answer}.`;
     }   else {
-        responseEl.textContent = "Ez peezy :D";
+        responseEl.textContent = "You're right :D";
     }
     responseEl.setAttribute("class", "response");
-    
+    setTimeout(function () {
+        responseEl.setAttribute("response", "response hide")
+    }, 3000);
+    qIndex++;
     if (qIndex === questions.length) {
-        quizEnd();
+        gameEnd();
     } else {
         newQuestion();
     }
 }
 
-function quizEnd()
+function timeDown() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+        gameEnd();
+    }
+}
+
+function gameEnd() {
+    clearInterval(timerID);
+    var gameEndEl = document.getElementById("container-end");
+    gameEndEl.removeAttribute("class");
+    var finalScoreEl = document.getElementById("final-score")
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
+}
+
+
+
+// Initializations 
+
+endplayBtn.addEventListener("click", gameEnd);
+
+document.getElementById("play-start").addEventListener("click", gameStart);
